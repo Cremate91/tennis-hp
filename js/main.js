303fe96cc5b75674fd12a1cf59bf82ac6d4f823a
Cremate91 <andreas.burger91@gmail.com>
2016-10-25 23:03:52 +0200

    publish

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by andre on 23.10.2016.
+ */
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'secondPage', '3rdPage'],
+        css3: true
+    });
+});
+var onMapMouseleaveHandler = function(event) {
+    var that = $(this);
+    that.on('click', onMapClickHandler);
+    that.off('mouseleave', onMapMouseleaveHandler);
+    that.find('iframe').css("pointer-events", "none");
+}
+var onMapClickHandler = function(event) {
+    var that = $(this);
+    // Disable the click handler until the user leaves the map area
+    that.off('click', onMapClickHandler);
+    // Enable scrolling zoom
+    that.find('iframe').css("pointer-events", "auto");
+    // Handle the mouse leave event
+    that.on('mouseleave', onMapMouseleaveHandler);
+}
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler);
