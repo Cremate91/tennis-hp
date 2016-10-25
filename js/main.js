12090ba6d219fdec7b855f5cce358f1cb4b15055
Cremate91 <andreas.burger91@gmail.com>
2016-10-25 23:22:15 +0200

    responsiv scroll

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['firstPage', 'secondPage', '3rdPage'],
-        css3: true
+        css3: true,
+        responsiveWidth: 1100
     });
 });
-var onMapMouseleaveHandler = function(event) {
+var onMapMouseleaveHandler = function() {
     var that = $(this);
     that.on('click', onMapClickHandler);
     that.off('mouseleave', onMapMouseleaveHandler);
     that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
+};
+var onMapClickHandler = function() {
     var that = $(this);
     // Disable the click handler until the user leaves the map area
     that.off('click', onMapClickHandler);
@@ -21,6 +22,6 @@ var onMapClickHandler = function(event) {
     that.find('iframe').css("pointer-events", "auto");
     // Handle the mouse leave event
     that.on('mouseleave', onMapMouseleaveHandler);
-}
+};
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
